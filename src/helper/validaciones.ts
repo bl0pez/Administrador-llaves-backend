0289dec36a6ef882b72e86d9f93aee1c79d58eee
bl0pez <blopez4434@gmail.com>
2023-07-03 09:19:44 -0400

    mejorando validacion de campos

--- src/helper/validaciones.ts
+++ src/helper/validaciones.ts
@@ -6,6 +6,6 @@ type ValidateFields = {
 
 // Expresion regular para validar que solo se permitan letras y numeros
 export const validarLetrasNumeros:ValidateFields = {
-    expresion: /^[a-zA-Z0-9]+$/,
+    expresion: /^[a-zA-Z0-9áéíóúÁÉÍÓÚ,]+$/,
     msg: 'Solo se permiten letras y numeros'
 }
